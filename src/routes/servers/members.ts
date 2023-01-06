fab5b7cf0f90ef15087e8569d66d570e033ae0f4
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-06 14:33:19 -0500

    actually fetch members

--- src/routes/servers/members.ts
+++ src/routes/servers/members.ts
@@ -1,8 +1,17 @@
 import { GET } from "../..";
+import mapMember from "../../conversion/member";
+import { ulidToSnowflake } from "../../conversion/ulid";
+import mapUser from "../../conversion/user";
 
 GET("-/servers/{target}/members", async (params) => {
-  return {
-    users: [],
-    members: [],
-  };
+  try {
+    const server = params.authenticated.guilds.cache.get(ulidToSnowflake((<any>params).target));
+    const members = await server.members.fetch({ withPresences: true });
+    return {
+      users: members.map((m) => mapUser(m.user)),
+      members: members.map(mapMember),
+    };
+  } catch (err) {
+    return { users: [], members: [] };
+  }
 });
