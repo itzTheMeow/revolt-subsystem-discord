bbafad9733b7ff27060b31b640e2bdb1544a2988
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-07 01:24:54 -0500

    fix unreads

--- src/routes/sync/unreads.ts
+++ src/routes/sync/unreads.ts
@@ -1,5 +1,16 @@
+import { Client } from "discord.js";
+import { ulid } from "ulid";
 import { GET } from "../..";
+import { snowflakeToULID } from "../../conversion/ulid";
 
-GET("/sync/unreads", async () => {
-  return [];
+GET("/sync/unreads", async (p: { authenticated: Client }) => {
+  return p.authenticated.channels.cache.map((c) => ({
+    _id: {
+      channel: snowflakeToULID(c.id),
+      user: snowflakeToULID(p.authenticated.user.id),
+    },
+    // 1 year
+    last_id: ulid(Date.now() + 3.156e10),
+    mentions: [],
+  }));
 });
