99b7bab106bc195ee5f803b6df529d4557382dcc
Meow <me@itsmeow.cat>
2023-04-14 19:26:26 -0400

    fix

--- src/routes/auth/session/logout.ts
+++ src/routes/auth/session/logout.ts
@@ -2,6 +2,6 @@ import { POST } from "../../..";
 import { destroyClient } from "../../../auth";
 
 POST("/auth/session/logout", async (params) => {
-  if (params.authenticated) destroyClient(params.authenticated);
+  destroyClient(params.authenticated);
   return undefined;
 });
