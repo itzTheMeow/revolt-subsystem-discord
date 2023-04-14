0cfa429bb9bd7a387555feb0d30a33941f0ae65a
Meow <me@itsmeow.cat>
2023-04-14 19:26:23 -0400

    bridge server ordering

--- src/routes/sync/settings/fetch.ts
+++ src/routes/sync/settings/fetch.ts
@@ -1,5 +1,22 @@
 import { POST } from "../../..";
+import { snowflakeToULID } from "../../../conversion/ulid";
 
-POST("/sync/settings/fetch", async (e) => {
-  return {}; //TODO:
+POST("/sync/settings/fetch", async (params) => {
+  const syncSettings: Record<string, Record<string, any> | any[]> = {
+    ordering: {
+      servers: params.authenticated.settings.guildFolder.cache
+        .map((f) => f.guild_ids)
+        .flat(1)
+        .map(snowflakeToULID),
+    },
+  };
+
+  if (params.keys?.length) {
+    Object.keys(syncSettings).forEach((k) => !params.keys.includes(k) && delete syncSettings[k]);
+  }
+
+  return Object.entries(syncSettings).reduce(
+    (settings, e) => ({ ...settings, [e[0]]: [Date.now(), JSON.stringify(e[1])] }),
+    {}
+  );
 });
